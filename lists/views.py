884c1a68a41ee4f64bdd8644cff887ca2dbd2d2b
Nobody <nobody@nobody.com>
2019-01-07 00:44:56 +0800

    enforce model validation in list view

--- lists/views.py
+++ lists/views.py
@@ -8,7 +8,7 @@ def home_page(request):
 
 def new_list(request):
     list_ = List.objects.create()
-    item = Item.objects.create(text=request.POST['item_text'], list=list_)
+    item = Item(text=request.POST['item_text'], list=list_)
     try:
         item.full_clean()
         item.save()
